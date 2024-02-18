e60194ad72fb19449987776fc2b3d5b9366cd6e2
OsMax <www.osmax@gmail.com>
2024-02-18 18:15:28 +0200

    fix favorite scroll

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,13 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { selectFavorites } from "redux/selectors";
 import { CarsList } from "components/CarsList/CarsList";
-import css from "./FavoritePage.module.css"
+import css from "./FavoritePage.module.css";
 
 const FavoritePage = () => {
   const cars = useSelector(selectFavorites);
   // console.log(cars);
   return (
     <>
+      <div className={css.stub}></div>
       <div className={css.favorContainer}>
         <CarsList cars={cars} />
       </div>
